e9dd65c13962957ae05749024f57cdee3176f48b
Jimmy Luo <1052065965@qq.com>
2022-12-27 11:57:01 +0800

    1.新增玩法：随机传送； 2.修复表单验证问题。

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # mymaze.github.io
 
-> 我的迷宫游戏
+> 我的迷宫游戏 [线上效果](https://mymaze.github.io/)
 
 ### 预览图
 
--- static/js/main.js
+++ static/js/main.js
@@ -1,85 +1,66 @@
-window.onload = function () {
-    const canvas = document.getElementById("canvas");
-    const context = canvas.getContext("2d");
+const
+    canvas = document.getElementById("canvas"),
+    context = canvas.getContext("2d");
 
-    const diamondSize = document.getElementById("diamondSize");
-    const startBtn = document.getElementById("startBtn");
+const
+    diamondSize = document.getElementById("diamondSize"),
+    randomTransferCheckBox = document.getElementById("randomTransferCheckBox"),
+    randomTransferCount = document.getElementById("randomTransferCount");
 
-    let maze = null;
+const
+    WALL = 0,
+    ROAD = 1,
+    TRANSFER = 2,
+    EXIT = 3,
+    PLAYER = 5;
 
-    startBtn.onclick = function () {
-        let space = parseInt(diamondSize.value);
+let maze = null;
 
-        let side = Math.floor((window.innerHeight - space * 2) / space);
-        side = side % 2 ? side : side - 1;
 
-        canvas.style.borderWidth = space + "px";
-        canvas.width = side * space;
-        canvas.height = side * space;
+function startGame() {
+    let space = parseInt(diamondSize.value);
 
-        maze = new Maze(context, side, side, space);
-        maze.start();
+    let transferCount = 0;
+    if (randomTransferCheckBox.checked) {
+        transferCount = parseInt(randomTransferCount.value);
     }
 
-    window.addEventListener("keydown", (event) => {
-        if (event.defaultPrevented) {
-            return; // Do nothing if event already handled
-        }
+    let side = Math.floor((window.innerHeight - space * 2) / space);
+    side = side % 2 ? side : side - 1;
 
-        switch (event.code) {
-            case "KeyS":
-            case "ArrowDown":
-                // Handle "back"
-                maze.move([0, 1]);
-                break;
-            case "KeyW":
-            case "ArrowUp":
-                // Handle "forward"
-                maze.move([0, -1]);
-                break;
-            case "KeyA":
-            case "ArrowLeft":
-                // Handle "turn left"
-                maze.move([-1, 0]);
-                break;
-            case "KeyD":
-            case "ArrowRight":
-                // Handle "turn right"
-                maze.move([1, 0]);
-                break;
-        }
+    canvas.style.borderWidth = space + "px";
+    canvas.width = side * space;
+    canvas.height = side * space;
 
-        if (event.code !== "Tab") {
-            // Consume the event so it doesn't get handled twice,
-            // as long as the user isn't trying to move focus away
-            // event.preventDefault();
-        }
-    }, true);
+    maze = new Maze(context, side, side, space, transferCount);
+    maze.start();
 
+    return false;
 }
 
 class Maze {
-    constructor(context, row, col, space) {
+    constructor(context, row, col, space, transferCount) {
         this.context = context;
         this.y = row;
         this.x = col;
         this.space = space;
-        // 方向：左、上、右、下
+        this.transferCount = transferCount;
+
         this.dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
         this.now = [];
         this.end = [];
         this.map = [];
-        // 图例
         this.legend = {
-            0: "black", // 围墙
-            1: "white", // 道路
-            3: "green", // 出口
-            5: "blue"   // 玩家
+            [WALL]: "black",
+            [ROAD]: "white",
+            [TRANSFER]: "yellow",
+            [EXIT]: "green",
+            [PLAYER]: "blue"
         }
     }
 
     resetMap() {
-        this.map = Array(this.y).fill().map(() => Array(this.x).fill(0));
+        this.map = Array(this.y).fill().map(() => Array(this.x).fill(WALL));
     }
 
     getMap(xy) {
@@ -88,6 +69,10 @@ class Maze {
 
     setMap(xy, value) {
         this.map[xy[1]][xy[0]] = value;
+    }
+
+    turnTo(xy, dir) {
+        return [xy[0] + dir[0], xy[1] + dir[1]];
     }
 
     random(min, max) {
@@ -104,20 +89,39 @@ class Maze {
         this.now = corner.splice(this.random(0, corner.length - 1), 1)[0];
         this.end = corner.splice(this.random(0, corner.length - 1), 1)[0];
 
-        this.setMap(this.now, 5);
-        this.setMap(this.end, 3);
+        this.setMap(this.now, PLAYER);
+        this.setMap(this.end, EXIT);
+    }
+
+    setRandomTransfer() {
+        if (!this.transferCount) return;
+
+        let count = 0;
+        while (count !== this.transferCount) {
+            let transferXY = [this.random(0, this.x - 1), this.random(0, this.y - 1)];
+            if (this.getMap(transferXY) === WALL) {
+                for (let curDir of this.dir) {
+                    let diamond = this.turnTo(transferXY, curDir);
+                    if (this.isSecurityScope(diamond) && this.getMap(diamond) === ROAD) {
+                        this.setMap(transferXY, TRANSFER);
+                        count++;
+                        break;
+                    }
+                }
+            }
+        }
     }
 
     prim() {
         let stack = [[0, 1], [1, 0]];
-        this.setMap([0, 0], 1);
+        this.setMap([0, 0], ROAD);
 
         while (stack.length > 0) {
             let curWall = stack.splice(this.random(0, stack.length - 1), 1)[0];
 
             let check = [];
             for (let curDir of this.dir) {
-                let road = [curWall[0] + curDir[0], curWall[1] + curDir[1]];
+                let road = this.turnTo(curWall, curDir);
                 if (this.isSecurityScope(road) && this.getMap(road)) {
                     check.push([curDir[0] * -1, curDir[1] * -1]);
                 }
@@ -125,13 +129,13 @@ class Maze {
 
             if (check.length === 1) {
                 let revDir = check.pop();
-                let newRoad = [curWall[0] + revDir[0], curWall[1] + revDir[1]];
+                let newRoad = this.turnTo(curWall, revDir);
 
-                this.setMap(curWall, 1);
-                this.setMap(newRoad, 1);
+                this.setMap(curWall, ROAD);
+                this.setMap(newRoad, ROAD);
 
                 for (let curDir of this.dir) {
-                    let newWall = [newRoad[0] + curDir[0], newRoad[1] + curDir[1]];
+                    let newWall = this.turnTo(newRoad, curDir);
                     if (this.isSecurityScope(newWall) && !this.getMap(newWall)) {
                         stack.push(newWall);
                     }
@@ -144,15 +148,22 @@ class Maze {
     move(dir) {
         let nextPos = [this.now[0] + dir[0], this.now[1] + dir[1]];
         if (this.isSecurityScope(nextPos) && this.getMap(nextPos)) {
-            if (this.getMap(nextPos) === 3) {
-                if (confirm("恭喜通关！\n是否继续以当前设置开始新的迷宫？")) {
-                    this.start();
-                }
-                return;
+            switch (this.getMap(nextPos)) {
+                case 2:
+                    while (true) {
+                        nextPos = [this.random(0, this.x - 1), this.random(0, this.y - 1)];
+                        if (this.getMap(nextPos) === ROAD) break;
+                    }
+                    break;
+                case 3:
+                    if (confirm("恭喜通关！\n是否继续以当前设置开始新的迷宫？")) {
+                        this.start();
+                    }
+                    return;
             }
 
-            this.setMap(this.now, 1);
-            this.setMap(nextPos, 5);
+            this.setMap(this.now, ROAD);
+            this.setMap(nextPos, PLAYER);
             this.now = nextPos;
 
             this.draw();
@@ -172,6 +183,41 @@ class Maze {
         this.resetMap();
         this.prim();
         this.setStartAndEnd();
+        this.setRandomTransfer();
         this.draw();
     }
 }
+
+window.addEventListener("keydown", (event) => {
+    if (event.defaultPrevented) return; // Do nothing if event already handled
+    if (!maze) return;
+
+    switch (event.code) {
+        case "KeyS":
+        case "ArrowDown":
+            // Handle "back"
+            maze.move([0, 1]);
+            break;
+        case "KeyW":
+        case "ArrowUp":
+            // Handle "forward"
+            maze.move([0, -1]);
+            break;
+        case "KeyA":
+        case "ArrowLeft":
+            // Handle "turn left"
+            maze.move([-1, 0]);
+            break;
+        case "KeyD":
+        case "ArrowRight":
+            // Handle "turn right"
+            maze.move([1, 0]);
+            break;
+    }
+
+    if (event.code !== "Tab") {
+        // Consume the event so it doesn't get handled twice,
+        // as long as the user isn't trying to move focus away
+        // event.preventDefault();
+    }
+}, true);
